ee10d272c9394bfb2b0fd7732f03933747b2e991
Jhon Alexander Suárez Sepúlveda <jhoals@hotmail.com>
2023-03-29 11:30:51 -0500

    Se adiciona sección 1 para ejercicio reloj y alarma

--- assets/components/index.dom.js
+++ assets/components/index.dom.js
@@ -1,7 +1,10 @@
 import hamburgerMenu from "./menu_hamburguesa.js";
+import { digitalClock, alarm } from "./reloj.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", e =>{
     hamburgerMenu(".panel-boton", ".panel", ".menu a");
+    digitalClock("#reloj","#activar-reloj","#descativar-reloj");
+    alarm("/assets/sounds/car-alarm.mp3","#activar-alarma","#desactivar-alarma")
 })
